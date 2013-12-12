da8a60f24c39ba52d3a6c9766d4a882c7ce73542
Denis Costa <deniscostadsc@gmail.com>
2013-12-12 10:57:48 -0200

    Compilando html.

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 sed -i 's/\(Data de última compilação:\).*$/'"\1 $(date +%d-%m-%Y)"'/g' marathon.rst
 
 rst2pdf marathon.rst
+rst2html marathon.rst > index.html
 
 sed -i 's/\(Data de última compilação:\).*$/\1/g' marathon.rst
