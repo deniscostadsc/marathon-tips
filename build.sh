c3c54709c6b4936d94df1b2e8441ee941162d8dc
Denis Costa <deniscostadsc@gmail.com>
2013-12-26 18:45:37 -0200

    Removendo coisas inuteis para o gh.

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sed -i 's/\(Data de última compilação:\).*$/'"\1 $(date +%d-%m-%Y)"'/g' marathon.rst
-
-rst2pdf marathon.rst
-rst2html marathon.rst > index.html
-
-sed -i 's/\(Data de última compilação:\).*$/\1/g' marathon.rst
